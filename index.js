f3f947832a843e0d4594a4c1f94925e8b6b54aa9
Ankit-Chitrakar <chitrakarankit2003@gmail.com>
2024-03-13 12:57:28 +0530

    first backend deployment on github

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Ankit Hello");
+// console.log("Ankit Hello");
+
+const express = require('express');
+require('dotenv').config()
+// import express from "express";
+const app = express();
+
+const port = process.env.PORT
+
+app.get('/', (req, res)=>{
+    res.send("Hello Ankit Chitrakar")
+})
+
+app.get('/me', (req, res)=>{
+    res.send("Tiasa Chitrakar")
+})
+
+app.get('/youtube', (req, res)=>{
+    res.send("Hello Youtube")
+})
+
+app.listen(port, ()=>{
+    console.log(`App listen on port ${port}`);
+})
